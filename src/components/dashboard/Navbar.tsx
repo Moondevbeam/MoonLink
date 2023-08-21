6a82d5f1cd0dd0d955b0abb7f3cca06f97ceb485
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-08-21 16:09:57 +0200

    fixed navbar username bug

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -18,31 +18,47 @@ function Navbar() {
   };
 
   useEffect(() => {
-    const userRef = firestore.collection('users').doc(auth.currentUser?.uid);
-    userRef.get().then((doc) => {
-      if (doc.exists) {
-        setUsername(doc.data()?.username || null);
+    const fetchUsername = async () => {
+      try {
+        const userRef = firestore.collection('users').doc(auth.currentUser?.uid);
+        const doc = await userRef.get();
+        if (doc.exists) {
+          setUsername(doc.data()?.username || null);
+        }
+      } catch (error) {
+        console.error('Error fetching username:', error);
       }
+    };
+
+    fetchUsername(); // Call the function to fetch the username
+
+    // Subscribe to changes in authentication state (user login/logout)
+    const unsubscribe = auth.onAuthStateChanged(() => {
+      fetchUsername(); // Fetch username when authentication state changes
     });
+
+    return () => {
+      unsubscribe(); // Clean up the auth state change listener
+    };
   }, []);
 
   return (
     <nav className="bg-gray-500 py-4">
       <div className="container mx-auto flex justify-start items-center space-x-3 font-semibold">
-      {username && (
-            <span className="text-white text-lg rounded bg-red-300 p-2">{username}</span>
-          )}
-          <div className="container mx-auto flex justify-end items-center space-x-3 font-semibold">
-        <Link to="/dashboard" className="text-white text-lg font-semibold rounded bg-purple-600 hover:bg-purple-700 px-4 py-2">
-          Dashboard
-        </Link>
-        <div className="flex items-center space-x-3">
-          <Link to="/settings" className="text-white text-lg font-semibold rounded bg-blue-600 hover:bg-blue-700 px-4 py-2">
-            Settings
+        {username && (
+          <span className="text-white text-lg rounded bg-red-300 p-2">{username}</span>
+        )}
+        <div className="container mx-auto flex justify-end items-center space-x-3 font-semibold">
+          <Link to="/dashboard" className="text-white text-lg font-semibold rounded bg-purple-600 hover:bg-purple-700 px-4 py-2">
+            Dashboard
           </Link>
-          <button className="text-white text-lg rounded bg-blue-600 hover:bg-blue-700 px-4 py-2" onClick={handleSignOut}>
-            Log Out
-          </button>
+          <div className="flex items-center space-x-3">
+            <Link to="/settings" className="text-white text-lg font-semibold rounded bg-blue-600 hover:bg-blue-700 px-4 py-2">
+              Settings
+            </Link>
+            <button className="text-white text-lg rounded bg-blue-600 hover:bg-blue-700 px-4 py-2" onClick={handleSignOut}>
+              Log Out
+            </button>
           </div>
         </div>
       </div>
