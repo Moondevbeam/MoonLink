49fd9526ec7fcf20b979d5163d906da540f474e8
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-08-21 23:59:33 +0200

    fixed animate bounce in adfooter

--- src/components/utils/AdFooter.tsx
+++ src/components/utils/AdFooter.tsx
@@ -7,15 +7,15 @@ function AdFooter() {
     <footer className="bg-transparent text-white py-8 fixed bottom-0 left-0 w-full text-center">
       <div className="container mx-auto">
         <p className="text-lg font-semibold mb-4 text-black">powered by MoonLink</p>
-        <p className="text-sm mb-2">
+        <button className="text-sm p-4">
           <a
             href="https://moon-link.vercel.app"
-            className="text-blue-500 hover:underline flex items-center justify-center animate-bounce hover:animate-none"
+            className="text-black hover:underline flex items-center justify-center animate-bounce hover:animate-none"
           >
             Sign Up now!
             <FontAwesomeIcon icon={faArrowRight} className="ml-2" />
           </a>
-        </p>
+        </button>
       </div>
     </footer>
   );
